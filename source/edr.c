3eac13c475ec9a42313cdde014bd79ea815fc5aa
Henrik Lindblom <henrik.lindblom@aalto.fi>
2012-06-18 13:31:50 +0300

    The amplifier gain is an integer between 1 and 16. Changed integer types to reflect the witdth of the number.

--- source/edr.c
+++ source/edr.c
@@ -1,3 +1,5 @@
+
+#define GAIN_MASK 0x0F /* 0000 1111, 16 in decimal. 16 == max gain available */
 /*
   ReadAnalogVoltage
   Reads an analog input on pin 0, converts it to voltage, and prints the result to the 
@@ -13,21 +15,21 @@ void setup() {
 }
 
 float EDRvalue = 0;
-float gain = 1;
+uint8_t gain = 1;
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin 0: EDR-pinni?
-  int sensorValue = analogRead(A0);
+  uint16_t sensorValue = analogRead(A0);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
   // print out the value you read:
-  Serial.println(voltage);
+  Serial.println(voltage); // does not print expected results.
   if (voltage > EDRvalue){
-      gain = gain-0.01;
+      gain = (gain - 1) & GAIN_MASK;
   }
   if (voltage < EDRvalue){
-      gain = gain+0.01;
+      gain = (gain + 1) & GAIN_MASK;
   }
   EDRvalue = voltage;
 }
